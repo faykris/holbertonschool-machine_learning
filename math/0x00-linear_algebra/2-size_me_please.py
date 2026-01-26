ab61fb6c7d6d2a9827109defe330a9273f79006b
Cristian Fayber Pinzón Capera <faykris28@gmail.com>
2026-01-26 15:07:00 -0500

    Remove import numpy

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 2. Size Me Please
 """
-import numpy as np
 
 
 def matrix_shape(matrix):
     """size of the matrix"""
-    return list(np.shape(matrix))
+    return list(matrix)
 
