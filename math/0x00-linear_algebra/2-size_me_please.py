cd0bfcc66050108677485f770ff78b9ec45703a0
Cristian Fayber Pinzón Capera <faykris28@gmail.com>
2026-01-26 12:54:06 -0500

    install libs and gitignore

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -3,11 +3,11 @@
     Task 2: Size Me Please
     Return size of matrix
 """
-
+import numpy as np
 
 def matrix_shape(matrix):
     """
     :param matrix: array double
     :return: size of the matrix
     """
-    return numpy.shape(matrix)
+    return np.shape(matrix)
