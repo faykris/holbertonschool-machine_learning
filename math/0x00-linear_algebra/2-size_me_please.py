271004da0dd9fc36446895c1de9b996b58fd20bd
Cristian Fayber Pinzón Capera <faykris28@gmail.com>
2026-01-26 10:58:55 -0500

    Add permissions task 2

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -4,12 +4,11 @@
     Return size of matrix
 """
 
-import numpy as np
 
 def matrix_shape(matrix):
     """
     :param matrix: array double
     :return: size of the matrix
     """
-    return np.shape(matrix)
+    return numpy.shape(matrix)
 
